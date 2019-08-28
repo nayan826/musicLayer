a2d7db0b236fb1abf753d8ff5f150fb2972253ba
Nayan Sharma <nayansharma52@gmail.com>
2019-08-28 21:17:33 +0530

    Production deploy

---
+++ build/precache-manifest.42986f73163ecb7ba2d4a7ee1a69b71e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c32fa6f23699dbce73d0f3a5e90e6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c26aaf2e2aa5b3afbfa",
+    "url": "/static/css/main.a5ec6406.chunk.css"
+  },
+  {
+    "revision": "b82af258d7eb2c2493dd",
+    "url": "/static/js/2.00f6b77f.chunk.js"
+  },
+  {
+    "revision": "8c26aaf2e2aa5b3afbfa",
+    "url": "/static/js/main.cdac0cdf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0cbf729c78f6ecce45a2c94433447b0a",
+    "url": "/static/media/Euphoria.0cbf729c.mp3"
+  },
+  {
+    "revision": "5b61b54ec45fb263ea303a3da6f1f1a0",
+    "url": "/static/media/FUN - SOME NIGHTS.5b61b54e.mp3"
+  },
+  {
+    "revision": "df5e1e16097a776b48f42c79420e7a6b",
+    "url": "/static/media/FUN - WE ARE YOUNG.df5e1e16.mp3"
+  },
+  {
+    "revision": "e803281fdf3c24d45576a2583a48d048",
+    "url": "/static/media/Serendipity.e803281f.mp3"
+  },
+  {
+    "revision": "50a353c1ed92087c618ef2b7ca082c2f",
+    "url": "/static/media/boy[1030].50a353c1.png"
+  },
+  {
+    "revision": "b1a126d992928933052dd6f8df8c0f43",
+    "url": "/static/media/cd[1028].b1a126d9.png"
+  }
+]);
