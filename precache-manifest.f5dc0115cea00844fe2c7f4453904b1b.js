9d60177d8508ad3ab773011909252e5a4ac3bc82
Nayan Sharma <nayansharma52@gmail.com>
2019-08-28 22:11:52 +0530

    Updates

---
+++ precache-manifest.f5dc0115cea00844fe2c7f4453904b1b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddd9260de9b02f23355d6084f15757e",
+    "url": "/musicLayer/index.html"
+  },
+  {
+    "revision": "8c26aaf2e2aa5b3afbfa",
+    "url": "/musicLayer/static/css/main.a5ec6406.chunk.css"
+  },
+  {
+    "revision": "b82af258d7eb2c2493dd",
+    "url": "/musicLayer/static/js/2.00f6b77f.chunk.js"
+  },
+  {
+    "revision": "8c26aaf2e2aa5b3afbfa",
+    "url": "/musicLayer/static/js/main.cdac0cdf.chunk.js"
+  },
+  {
+    "revision": "04411e99a31fb755a35c",
+    "url": "/musicLayer/static/js/runtime~main.22b85861.js"
+  },
+  {
+    "revision": "0cbf729c78f6ecce45a2c94433447b0a",
+    "url": "/musicLayer/static/media/Euphoria.0cbf729c.mp3"
+  },
+  {
+    "revision": "5b61b54ec45fb263ea303a3da6f1f1a0",
+    "url": "/musicLayer/static/media/FUN - SOME NIGHTS.5b61b54e.mp3"
+  },
+  {
+    "revision": "df5e1e16097a776b48f42c79420e7a6b",
+    "url": "/musicLayer/static/media/FUN - WE ARE YOUNG.df5e1e16.mp3"
+  },
+  {
+    "revision": "e803281fdf3c24d45576a2583a48d048",
+    "url": "/musicLayer/static/media/Serendipity.e803281f.mp3"
+  },
+  {
+    "revision": "50a353c1ed92087c618ef2b7ca082c2f",
+    "url": "/musicLayer/static/media/boy[1030].50a353c1.png"
+  },
+  {
+    "revision": "b1a126d992928933052dd6f8df8c0f43",
+    "url": "/musicLayer/static/media/cd[1028].b1a126d9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nayan826/musicLayer.git/precache-manifest.285a2e6a53eef40661432b4d110d2a56.js"
+  "/musicLayer/precache-manifest.f5dc0115cea00844fe2c7f4453904b1b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nayan826/musicLayer.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/musicLayer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
